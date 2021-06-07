6d3249b94bae1aca154110f8a6d66d6033d6484e
cnbaixf <747497159@qq.com>
2021-06-07 23:02:40 +0800

    eee

---
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txButton8 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txButton1 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txButton2 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txButton3 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txButton4 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txButton5 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txButton6 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txButton7 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txButton9 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(123, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(221, 377);
+            this.panel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txButton1);
+            this.flowLayoutPanel1.Controls.Add(this.txButton2);
+            this.flowLayoutPanel1.Controls.Add(this.txButton3);
+            this.flowLayoutPanel1.Controls.Add(this.txButton4);
+            this.flowLayoutPanel1.Controls.Add(this.txButton5);
+            this.flowLayoutPanel1.Controls.Add(this.txButton6);
+            this.flowLayoutPanel1.Controls.Add(this.txButton7);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 35);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(796, 34);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // txButton8
+            // 
+            this.txButton8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton8.Image = null;
+            this.txButton8.Location = new System.Drawing.Point(768, 2);
+            this.txButton8.Name = "txButton8";
+            this.txButton8.Size = new System.Drawing.Size(30, 28);
+            this.txButton8.TabIndex = 4;
+            this.txButton8.Text = "×";
+            this.txButton8.UseVisualStyleBackColor = true;
+            // 
+            // txButton1
+            // 
+            this.txButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txButton1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txButton1.Image = null;
+            this.txButton1.Location = new System.Drawing.Point(3, 3);
+            this.txButton1.Name = "txButton1";
+            this.txButton1.Size = new System.Drawing.Size(100, 28);
+            this.txButton1.TabIndex = 0;
+            this.txButton1.Text = "Animation";
+            this.txButton1.UseVisualStyleBackColor = true;
+            // 
+            // txButton2
+            // 
+            this.txButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton2.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txButton2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txButton2.Image = null;
+            this.txButton2.Location = new System.Drawing.Point(109, 3);
+            this.txButton2.Name = "txButton2";
+            this.txButton2.Size = new System.Drawing.Size(100, 28);
+            this.txButton2.TabIndex = 1;
+            this.txButton2.Text = "Movie";
+            this.txButton2.UseVisualStyleBackColor = true;
+            // 
+            // txButton3
+            // 
+            this.txButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton3.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txButton3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txButton3.Image = null;
+            this.txButton3.Location = new System.Drawing.Point(215, 3);
+            this.txButton3.Name = "txButton3";
+            this.txButton3.Size = new System.Drawing.Size(100, 28);
+            this.txButton3.TabIndex = 2;
+            this.txButton3.Text = "Comic";
+            this.txButton3.UseVisualStyleBackColor = true;
+            // 
+            // txButton4
+            // 
+            this.txButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton4.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txButton4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txButton4.Image = null;
+            this.txButton4.Location = new System.Drawing.Point(321, 3);
+            this.txButton4.Name = "txButton4";
+            this.txButton4.Size = new System.Drawing.Size(100, 28);
+            this.txButton4.TabIndex = 3;
+            this.txButton4.Text = "Picture";
+            this.txButton4.UseVisualStyleBackColor = true;
+            // 
+            // txButton5
+            // 
+            this.txButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton5.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txButton5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txButton5.Image = null;
+            this.txButton5.Location = new System.Drawing.Point(427, 3);
+            this.txButton5.Name = "txButton5";
+            this.txButton5.Size = new System.Drawing.Size(100, 28);
+            this.txButton5.TabIndex = 4;
+            this.txButton5.Text = "Novel";
+            this.txButton5.UseVisualStyleBackColor = true;
+            // 
+            // txButton6
+            // 
+            this.txButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton6.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txButton6.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txButton6.Image = null;
+            this.txButton6.Location = new System.Drawing.Point(533, 3);
+            this.txButton6.Name = "txButton6";
+            this.txButton6.Size = new System.Drawing.Size(100, 28);
+            this.txButton6.TabIndex = 5;
+            this.txButton6.Text = "Music";
+            this.txButton6.UseVisualStyleBackColor = true;
+            // 
+            // txButton7
+            // 
+            this.txButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton7.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txButton7.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txButton7.Image = null;
+            this.txButton7.Location = new System.Drawing.Point(639, 3);
+            this.txButton7.Name = "txButton7";
+            this.txButton7.Size = new System.Drawing.Size(100, 28);
+            this.txButton7.TabIndex = 6;
+            this.txButton7.Text = "Game";
+            this.txButton7.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.flowLayoutPanel2.Controls.Add(this.txButton9);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 75);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(103, 374);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // txButton9
+            // 
+            this.txButton9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txButton9.Image = null;
+            this.txButton9.Location = new System.Drawing.Point(3, 3);
+            this.txButton9.Name = "txButton9";
+            this.txButton9.Size = new System.Drawing.Size(100, 28);
+            this.txButton9.TabIndex = 0;
+            this.txButton9.Text = "全部";
+            this.txButton9.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.txButton8);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private TX.Framework.WindowUI.Controls.TXButton txButton1;
+        private TX.Framework.WindowUI.Controls.TXButton txButton2;
+        private TX.Framework.WindowUI.Controls.TXButton txButton3;
+        private TX.Framework.WindowUI.Controls.TXButton txButton4;
+        private TX.Framework.WindowUI.Controls.TXButton txButton5;
+        private TX.Framework.WindowUI.Controls.TXButton txButton6;
+        private TX.Framework.WindowUI.Controls.TXButton txButton7;
+        private TX.Framework.WindowUI.Controls.TXButton txButton8;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private TX.Framework.WindowUI.Controls.TXButton txButton9;
+    }
+}
+
